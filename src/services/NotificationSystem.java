789181feb00648dd5bdade6e0223040dff640aaa
OmarAyman1234 <omarayman9124@gmail.com>
2024-12-15 21:23:16 +0200

    Check if subscription is active

--- src/services/NotificationSystem.java
+++ src/services/NotificationSystem.java
@@ -1,8 +1,7 @@
 package services;
-import java.util.ArrayList;
-import modules.Coach;
 import modules.*;
 import java.io.*;
+import java.time.LocalDate;
 import java.util.*;
 
 public class NotificationSystem {
@@ -23,6 +22,42 @@ public class NotificationSystem {
         catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+    }
+
+    public static boolean isSubscriptionActive(String memberId) {
+        File membersFile = new File("resources\\Members.txt");
+        try {
+            Scanner membersScan = new Scanner(membersFile);
+
+            if(membersScan.hasNext()) {
+                membersScan.nextLine();
+
+                while(membersScan.hasNext()) {
+
+                    String[] parts = membersScan.nextLine().split("/");
+
+                    if(memberId.equals(parts[0])) {
+                        String expiryDateString = parts[5];
+
+                        String[] dateParts = expiryDateString.split("-");
+                        int expiryYear = Integer.parseInt(dateParts[0]);
+                        int expiryMonth = Integer.parseInt(dateParts[1]);
+                        int expiryDay = Integer.parseInt(dateParts[2]);
+
+                        LocalDate expiryDate = LocalDate.of(expiryYear, expiryMonth, expiryDay);
+                        LocalDate today = LocalDate.now();
+
+                        return expiryDate.isAfter(today);
+                    }
+                }
+            }
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        // If the member wasn't found
+        return false;
     }
 
     public static void sendMessage(String senderId, String receiverId, String message) throws FileNotFoundException {
